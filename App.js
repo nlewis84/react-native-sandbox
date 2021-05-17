51a1b777f54a86b84cdbea35e5ad1db21fc28d03
nllew <n.lewis.perc@gmail.com>
2021-05-17 15:59:03 -0500

    basic profile card

--- App.js
+++ App.js
@@ -1,31 +1,91 @@
 import React, { Component } from "react";
-import { Button, StyleSheet, View } from "react-native";
-import getStyleSheet from "./styles";
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            darkTheme: false,
-        };
-        this.toggleTheme = this.toggleTheme.bind(this);
-    }
-    toggleTheme() {
-        this.setState({ darkTheme: !this.state.darkTheme });
-    }
+import { Image, Text, StyleSheet, View } from "react-native";
+export default class App extends Component<{}> {
     render() {
-        const styles = getStyleSheet(this.state.darkTheme);
-        const backgroundColor = StyleSheet.flatten(
-            styles.container
-        ).backgroundColor;
         return (
             <View style={styles.container}>
-                <View style={styles.box}>
-                    <Button
-                        title={backgroundColor}
-                        onPress={this.toggleTheme}
-                    />
+                <View style={styles.cardContainer}>
+                    <View style={styles.cardImageContainer}>
+                        <Image
+                            style={styles.cardImage}
+                            source={require("./lake.jpg")}
+                        />
+                    </View>
+                    <View>
+                        <Text style={styles.cardName}>John Doe</Text>
+                    </View>
+                    <View style={styles.cardOccupationContainer}>
+                        <Text style={styles.cardOccupation}>
+                            ReactNative Developer
+                        </Text>
+                    </View>
+                    <View>
+                        <Text style={styles.cardDescription}>
+                            John is a really great JavaScript developer. He
+                            loves using JS to build React Native applications
+                            for iOS and Android.
+                        </Text>
+                    </View>
                 </View>
             </View>
         );
     }
 }
+
+const profileCardColor = "dodgerblue";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    cardContainer: {
+        alignItems: "center",
+        borderColor: "black",
+        borderWidth: 3,
+        borderStyle: "solid",
+        borderRadius: 20,
+        backgroundColor: profileCardColor,
+        width: 300,
+        height: 400,
+    },
+    cardImageContainer: {
+        alignItems: "center",
+        backgroundColor: "white",
+        borderWidth: 3,
+        borderColor: "black",
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginTop: 30,
+        paddingTop: 2,
+    },
+    cardImage: {
+        width: 110,
+        height: 110,
+        borderRadius: 55,
+    },
+    cardName: {
+        color: "white",
+        fontSize: 24,
+        marginTop: 30,
+        fontWeight: 700,
+    },
+    cardOccupationContainer: {
+        borderColor: "black",
+        borderBottomWidth: 3,
+    },
+    cardOccupation: {
+        marginTop: 10,
+        marginBottom: 10,
+        fontWeight: 700,
+    },
+    cardDescription: {
+        marginTop: 10,
+        marginRight: 40,
+        marginLeft: 40,
+        marginBottom: 10,
+        fontStyle: "italic",
+    },
+});
